ea036a2ee2f397a40d8a5990e4908ade267b380c
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2023-11-03 14:08:48 -0500

    diseño de la vista proyectos terminado

--- app/components/headerProject.tsx
+++ app/components/headerProject.tsx
@@ -3,13 +3,20 @@ import React from 'react'
 function HeaderProject() {
   return (
     
-    <div className='flex items-center py-5'>
+    <div className='flex items-center justify-between py-5'>
+      <div className='flex py-5 items-center'>
         <input 
             type="text" 
             placeholder="Buscar"
-            className="mt-1 p-2 block w-[20%] rounded-md bg-gray-100 border border-[#38c2cf] text-[#313338] focus:outline-none"
+            className="mt-1 p-2 block w-[300px] rounded-md bg-gray-100 border border-[#38c2cf] text-[#313338] focus:outline-none"
         />
         <i className="ri-more-2-line text-[#131415] font-black text-2xl cursor-pointer"></i>
+      </div>
+      <div>
+        <button className='btn bg-[#38c2cf] text-white px-2 py-1 border-none rounded-lg cursor-pointer hover:bg-[#7ecdd4] mr-6'>
+            Crear una nueva tarea
+        </button>
+      </div>
     </div>
   )
 }
